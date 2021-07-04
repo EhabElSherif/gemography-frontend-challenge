342c59a100befcc9022de3879bc9d0e51ca3aa03
Ehab El-Sherif <ehabraelsherif@gmail.com>
2021-07-04 05:21:16 +0200

    add pagination to ReposList

--- src/components/ReposList/index.js
+++ src/components/ReposList/index.js
@@ -1,11 +1,11 @@
 import './ReposList.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import RepoItem from '../RepoItem'
 
 export default function ReposList() {
+    const list_ref = useRef(null)
     const [repos,setRepos] = useState([])
-    const [total_count,setTotalCount] = useState(0)
-    const [page,setPage] = useState(0)
+    const [page,setPage] = useState(1)
     
     const fetchRepos = async (page)=>{
         var today = new Date()
@@ -13,24 +13,24 @@ export default function ReposList() {
         const creation_date = new Date(prior_date).toISOString().substr(0,10)
 
         const response = await fetch(`https://api.github.com/search/repositories?q=created:>${creation_date}&sort=stars&order=desc&page=${page}`)
-        const {total_count,items} = await response.json()
+        const {items} = await response.json()
         
-        setTotalCount(total_count)
 
-        setRepos(items.map(item=>({
+        setRepos([
+            ...repos,
+            ...(items?.map(item=>({
                 id: item.id,
                 name: item.name,
                 description: item.description,
                 stars_count: item.stargazers_count,
                 issues_count: item.open_issues_count,
                 created_at: (new Date(today - new Date(item.created_at))).getDate(),
-                url: item.html_url,
                 owner:{
                     username: item.owner.login,
                     avatar: item.owner.avatar_url
                 }
-            })
-        ))
+            }))||[])
+        ])
         
     }
 
@@ -38,14 +38,27 @@ export default function ReposList() {
         fetchRepos(page)
     },[page])
 
+    useEffect(()=>{
+        const handleHitBottom = (event) => {
+            if (list_ref.current.scrollHeight - 0.2*list_ref.current.offsetHeight <= list_ref.current.scrollTop + list_ref.current.offsetHeight) {
+                setPage(page+1)
+            }
+        }
+        list_ref.current.addEventListener('scroll', handleHitBottom)
+
+        return () => {
+            list_ref.current.removeEventListener('scroll', handleHitBottom)
+        }
+    },[page])
+
     const renderRepos = ()=>{
         return repos.map(repo=>(
             <RepoItem key={repo.id} repo={repo} />
         ))
     }
-
+    
     return (
-        <div className="ReposList">
+        <div ref={list_ref} className="ReposList">
             {renderRepos()}
         </div>
     )
